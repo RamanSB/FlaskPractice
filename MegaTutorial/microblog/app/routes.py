e02b8a22a6a8855b7dcc571e127ae96dc38da13e
RamanSB <ramansb100@gmail.com>
2019-03-26 00:11:54 +0000

    Template files added, base templates used and derived templates have inherited - Chapter 2

--- MegaTutorial/microblog/app/routes.py
+++ MegaTutorial/microblog/app/routes.py
@@ -6,10 +6,27 @@ Created on Mon Mar 25 22:20:52 2019
 @author: RamanSB
 """
 
+from flask import render_template
 from app import app #from the app package import the app variable (the flask instance)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Galeilo Galeilili'},
+            'body': 'Eureka, the weight of the gold crown is equal to the fluid displaced!'
+        }
+    ]
+    
+    return render_template('index.html', title='Home', user=user, posts=posts)
