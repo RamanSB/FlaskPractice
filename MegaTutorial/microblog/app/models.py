2bb3f997622ad28be724ea243c13d489c227de57
RamanSB <ramansb100@gmail.com>
2019-03-29 19:45:46 +0000

    Registration form & user login completed, FlaskForm from flask_wtf used + validators

--- MegaTutorial/microblog/app/models.py
+++ MegaTutorial/microblog/app/models.py
@@ -8,8 +8,11 @@ Created on Wed Mar 27 17:51:26 2019
 
 from app import db
 from datetime import datetime
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -18,6 +21,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
     
 class Post(db.Model):
@@ -28,3 +37,7 @@ class Post(db.Model):
     
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+    
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
