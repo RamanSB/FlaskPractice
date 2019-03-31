2d4123fcdcb307c1a8bb51ea9a6893cdc11efe9f
RamanSB <ramansb100@gmail.com>
2019-03-31 15:55:33 +0100

    Added Logging functionality, log files now created at logs/microblog.log

--- MegaTutorial/microblog/app/__init__.py
+++ MegaTutorial/microblog/app/__init__.py
@@ -6,11 +6,14 @@ Created on Mon Mar 25 22:10:00 2019
 @author: RamanSB
 """
 
+import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,4 +23,20 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 
-from app import routes, models
+if not app.debug:
+    # ...
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+
+from app import routes, models, errors
--- MegaTutorial/microblog/app/models.py
+++ MegaTutorial/microblog/app/models.py
@@ -11,6 +11,7 @@ from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,7 +19,15 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
+
+    def avatar(self, size):
+            digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+            return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+                digest, size)
+            
     def __repr__(self):
         return '<User {}>'.format(self.username)    
     
@@ -37,6 +46,8 @@ class Post(db.Model):
     
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+    
+    
     
 @login.user_loader
 def load_user(id):
