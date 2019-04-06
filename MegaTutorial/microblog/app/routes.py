87f6f33ce9fff6d8352253be11baadeb2e91893a
RamanSB <ramansb100@gmail.com>
2019-04-06 23:15:39 +0100

    Facelift, using bootstrap - specifically Flask_Bootstrap extension

--- MegaTutorial/microblog/app/routes.py
+++ MegaTutorial/microblog/app/routes.py
@@ -8,33 +8,36 @@ Created on Mon Mar 25 22:20:52 2019
 
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db #from the app package import the app variable (the flask instance)
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app.models import User, Post
 from datetime import datetime
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'author': {'username': 'Galeilo Galeilili'},
-            'body': 'Eureka, the weight of the gold crown is equal to the fluid displaced!'
-        }
-    ]
-    
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if(form.validate_on_submit()):
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int) #extracts value of query param from URL using request object
+    posts = current_user.followed_posts().paginate(
+            page, app.config['POSTS_PER_PAGE'], False)
+    if(posts.has_next):
+        next_url = url_for('index', page=posts.next_num)
+    else:
+        next_url = None
+    if(posts.has_prev):
+        prev_url = url_for('index', page=posts.prev_num)
+    else:
+        prev_url = None
+        
+    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 
@@ -83,11 +86,14 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-            {'author':user, 'body': 'Test post #1'},
-            {'author':user, 'body': 'Test post #2'}
-        ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+            page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+    if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+    if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.before_request
 def before_request():
@@ -141,3 +147,21 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+            page, app.config['POSTS_PER_PAGE'], False)
+
+    if(posts.has_next):
+        next_url = url_for('explore', page=posts.next_num)
+    else:
+        next_url = None
+    if(posts.has_prev):
+        prev_url = url_for('explore', page=posts.prev_num)
+    else:
+        prev_url = None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
