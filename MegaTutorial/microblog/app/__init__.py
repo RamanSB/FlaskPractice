c9b824fde9889ab3a282362840b13c937b201f08
RamanSB <ramansb100@gmail.com>
2019-04-07 17:33:56 +0100

    Better Application Structure

--- MegaTutorial/microblog/app/__init__.py
+++ MegaTutorial/microblog/app/__init__.py
@@ -7,7 +7,7 @@ Created on Mon Mar 25 22:10:00 2019
 """
 
 import os
-from flask import Flask
+from flask import Flask, request, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -16,6 +16,7 @@ from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import RotatingFileHandler
 
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config.from_object(Config)
@@ -23,22 +24,40 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login' #flask_login requires to know which view function is responsible for logging users in.
+login.login_message = 'Please log in to access this page.'
 
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-if not app.debug:
-    # ...
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    bootstrap.init_app(app)
+    
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
-from app import routes, models, errors
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
+
+    return app
+
+from app import models
